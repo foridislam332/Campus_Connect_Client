65715ffcbc68494541ba37391549882d8c225a94
foridislam332 <foridislam991@gmail.com>
2023-07-23 19:13:56 +0600

    live server link added

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const useAxios = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://campus-connect-server-delta.vercel.app',
 });
 
 export default useAxios;
